f1d7d4a27e4ab252fbfb619daa34321c92a4ab6f
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-10 01:11:43 -0800

    update 18.6.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,36 +2,52 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-sed -i "s?<arg>-Werror<arg>??g" pom.xml
-
 # Build archive with jar and other files
 ${PYTHON} -u tools/createCompletionLists.py
 mvn -B -e clean compile package -DskipTests=True
 
 # Extract newly built archive
-tar xzf target/ltex-ls-${PKG_VERSION}.tar.gz -C .
+tar xzf target/ltex-ls-plus-${PKG_VERSION}.tar.gz -C .
 
 mkdir -p ${PREFIX}/bin
-mkdir -p ${PREFIX}/libexec/${PKG_NAME}
-cp -r ltex-ls-${PKG_VERSION}/* ${PREFIX}/libexec/${PKG_NAME}
+mkdir -p ${PREFIX}/libexec/ltex-ls
+cp -r ltex-ls-plus-${PKG_VERSION}/* ${PREFIX}/libexec/ltex-ls
 
 # Create bash and batch wrappers for ltex-ls and ltex-cli
 tee ${PREFIX}/bin/ltex-cli << EOF
-exec \${CONDA_PREFIX}/libexec/ltex-ls/bin/ltex-cli "\$@"
+exec \${CONDA_PREFIX}/libexec/ltex-ls/bin/ltex-cli-plus "\$@"
 EOF
 chmod +x ${PREFIX}/bin/ltex-cli
 
 tee ${PREFIX}/bin/ltex-ls << EOF
-exec \${CONDA_PREFIX}/libexec/ltex-ls/bin/ltex-ls "\$@"
+exec \${CONDA_PREFIX}/libexec/ltex-ls/bin/ltex-ls-plus "\$@"
 EOF
 chmod +x ${PREFIX}/bin/ltex-ls
 
 tee ${PREFIX}/bin/ltex-cli.cmd << EOF
-call %CONDA_PREFIX%\libexec\ltex-ls\bin\ltex-cli.bat %*
+call %CONDA_PREFIX%\libexec\ltex-ls\bin\ltex-cli-plus.bat %*
 EOF
 
 tee ${PREFIX}/bin/ltex-ls.cmd << EOF
-call %CONDA_PREFIX%\libexec\ltex-ls\bin\ltex-ls.bat %*
+call %CONDA_PREFIX%\libexec\ltex-ls\bin\ltex-ls-plus.bat %*
+EOF
+
+tee ${PREFIX}/bin/ltex-cli-plus << EOF
+exec \${CONDA_PREFIX}/libexec/ltex-ls/bin/ltex-cli-plus "\$@"
+EOF
+chmod +x ${PREFIX}/bin/ltex-cli-plus
+
+tee ${PREFIX}/bin/ltex-ls-plus << EOF
+exec \${CONDA_PREFIX}/libexec/ltex-ls/bin/ltex-ls-plus "\$@"
+EOF
+chmod +x ${PREFIX}/bin/ltex-ls-plus
+
+tee ${PREFIX}/bin/ltex-cli-plus.cmd << EOF
+call %CONDA_PREFIX%\libexec\ltex-ls\bin\ltex-cli-plus.bat %*
+EOF
+
+tee ${PREFIX}/bin/ltex-ls-plus.cmd << EOF
+call %CONDA_PREFIX%\libexec\ltex-ls\bin\ltex-ls-plus.bat %*
 EOF
 
 # Download licenses for dependencies
